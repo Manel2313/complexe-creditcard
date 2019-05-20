efee5e5278eaa8efb5924620c692d4daf52767b9
Manel2313 <djemal.manel@gmail.com>
2019-05-20 14:15:47 +0100

    Updates

---
+++ precache-manifest.b3ae40c89ea613f61d8fe0a8cffe5c38.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7215196f3527d3f6e3877f0fece12cc",
+    "url": "/complexe-creditcard/index.html"
+  },
+  {
+    "revision": "bfe0f59f2e76e5893a6c",
+    "url": "/complexe-creditcard/static/css/main.aa6d53c3.chunk.css"
+  },
+  {
+    "revision": "d2103c5e095edc1346e1",
+    "url": "/complexe-creditcard/static/js/2.a68ccf2f.chunk.js"
+  },
+  {
+    "revision": "bfe0f59f2e76e5893a6c",
+    "url": "/complexe-creditcard/static/js/main.cef20bb9.chunk.js"
+  },
+  {
+    "revision": "8b09f1199e2632ead570",
+    "url": "/complexe-creditcard/static/js/runtime~main.149feead.js"
+  },
+  {
+    "revision": "2c7243c765bf8fafda26fe878f7e2d1a",
+    "url": "/complexe-creditcard/static/media/background.2c7243c7.png"
+  },
+  {
+    "revision": "5ac573dff32d4a9852dbb7073948edf2",
+    "url": "/complexe-creditcard/static/media/master-card.5ac573df.png"
+  },
+  {
+    "revision": "8bc50203ffcbf69c7e5c754558d98325",
+    "url": "/complexe-creditcard/static/media/puce.8bc50203.png"
+  },
+  {
+    "revision": "8486189aadc4809ebd8edfe89452e223",
+    "url": "/complexe-creditcard/static/media/visa.8486189a.png"
+  }
+]);
